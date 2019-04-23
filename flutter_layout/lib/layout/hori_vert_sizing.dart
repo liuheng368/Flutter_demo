800039f2cb192ea79eea91ef9edf3498241ec5b4
liuheng <liuheng02@imdada.cn>
2019-04-23 17:58:49 +0800

    [ADD]exercise-1

---
+++ flutter_layout/lib/layout/hori_vert_sizing.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+ 
+ class HoriVertSizingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HoriVertSizingPage App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('HoriVertSizingPage App Bar'),
+        ),
+        body: Center(
+          child: (
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Image.asset('assets/images/small-pic-1.jpg',fit: BoxFit.cover),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Image.asset('assets/images/small-pic-2.jpg',fit: BoxFit.cover),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Image.asset('assets/images/small-pic-3.jpg',fit: BoxFit.cover),
+                ),
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
